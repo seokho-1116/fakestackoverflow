5ab876a75bd1b722f553903ef17a88624d4340ea
seokho <seokho7799@gmail.com>
2025-02-19 23:24:25 +0900

    feat : first commit

---
+++ src/main/java/com/example/fake_stackoverflow/FakeStackoverflowApplication.java
@@ -0,0 +1,13 @@
+package com.example.fake_stackoverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeStackoverflowApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FakeStackoverflowApplication.class, args);
+  }
+
+}
